36fef0dad4d0109ff2fd63bb5fd732d1eab8e2b6
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-05-07 11:13:07 +0900

    자바 형변환 String -> int 추가

---
+++ 01_Variable/src/com/kh/variable/ex/String형변환.java
@@ -0,0 +1,15 @@
+package com.kh.variable.ex;
+/*
+	String -> 숫자, 실수형으로 형변환
+	
+	숫자
+	Integer.parseInt(변환할 String 변수);
+	
+*/
+public class String형변환 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
